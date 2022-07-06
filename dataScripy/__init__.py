e348df9c4bedac30dc2a391aecc58ad164f60afb
zhanghd <zhanghd@neocrm.com>
2022-07-06 17:46:47 +0800

    导入包

--- dataScripy/__init__.py
+++ dataScripy/__init__.py
@@ -1,2 +1,3 @@
 from .stock import *
 from .stockCode import *
+from .pandas_table import *
--- dataScripy/pandas_table.py
+++ dataScripy/pandas_table.py
@@ -7,10 +7,20 @@ import pandas as pd
 
 def get_profit_by_code(code,num =0):
 
+#url = https://q.stock.sohu.com/cn/600597/index.shtml
+
     url = f'https://q.stock.sohu.com/cn/{code}/index.shtml'
     # index:0 代表利润 2 代表业务构成
     tb = pd.read_html(url)[num]  # 经观察发现所需表格是网页中第4个表格，故为[3]
+
     # tb.to_csv(r'C:\Users\xxx\Desktop\1.csv', mode='a', encoding='utf_8_sig', header=1, index=0)
     # print('第' + str(i) + '页抓取完成')
-    print(tb)
 
+    # print(tb)
+    return tb
+
+if __name__ == '__main__':
+    df = get_profit_by_code(600597,0)
+    print(df)
+    print(df.iat[3, 2])
+    print(df.iat[2, 2])
