771d75dee66ecf2ce53d51e2f88fcaf2ed82d5a2
haodi <changgaoyou@163.com>
2022-07-22 22:30:25 +0800

    test

--- dataScripy/finance_by_code.py
+++ dataScripy/finance_by_code.py
@@ -14,6 +14,7 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
 }
 
+# 测试
 resp = req.get(url,headers)
 resp.encoding = 'utf-8'
 html = resp.text
